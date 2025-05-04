4ea303030843ecf39f33e595b8476473c3d1be23
zein <Zcode0430@gmail.com>
2025-05-04 11:27:04 -1200

    Your commit message

---
+++ components/PageBuilder.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import type { PageQueryResult } from "~/sanity/types";
+
+type PageBuilderBlock = NonNullable<
+  NonNullable<PageQueryResult>["pageBuilder"]
+>[number];
+
+defineProps({
+  blocks: {
+    type: Array as () => PageBuilderBlock[],
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <component
+    v-for="(block, index) in blocks"
+    :key="block._key"
+    :is="resolveComponent(block._type) || 'div'"
+    :block="block"
+  >
+    <div v-if="!resolveComponent(block._type)">
+     كنكوصه ميديا نسخ تجريبية
+    </div>
+  </component>
+</template>
